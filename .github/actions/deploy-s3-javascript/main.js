ec649b7c247902cd6da0aec4e3780e0ef53ad1c0
Tetiana Skazhenyk <tetiana.skazhenyk@volvocars.com>
2024-05-21 12:05:58 +0200

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,10 +9,11 @@ function run() {
   const distFolder = core.getInput("dist-folder", { required: true });
 
   // 2. Upload files
-  const s3uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`)
+  const s3uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`);
 
-  core.notice("Hello from my custom Javascript Action!");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl); //::set-output
 }
 
 run();
